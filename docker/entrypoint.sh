1b0f31699d167be833966a3492e1f464c3b8e9e6
greenplugin <wformps@gmail.com>
2021-01-13 00:10:39 +0300

    add some configuration

---
+++ docker/entrypoint.sh
@@ -0,0 +1,5 @@
+cd "$STEAMCMDDIR" || exit
+./steamcmd.sh +login anonymous +force_install_dir "$HOMEDIR"/bm +app_update 346680 +validate +quit
+
+cd "$HOMEDIR"/bm || exit
+/home/steam/bm/srcds_run -game bms +hostname 'Bm clean server' +map gasworks +maxplayers 4
